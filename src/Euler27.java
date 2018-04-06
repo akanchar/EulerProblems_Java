1c15705e0e536588d6e5f01adc7abd7b885ae32f
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-04-05 22:26:02 -0500

    ADD - Completed Euler 27

--- src/Euler27.java
+++ src/Euler27.java
@@ -1,10 +1,40 @@
+
 public class Euler27
 {
     public static void main(String[] args)
     {
-        int a = 1, b = 41;
-
-        for(int i = 0; i < )
-
+        int a, b, count, saveA = 0, saveB = 0, saveCount = 0;
+        boolean isPrime;
+        // Looping for different values of a and b coefficients
+        for(a = -1000;a <= 1000; a++) {
+            for (b = -1000; b <= 1000; b++) {
+                count = 0;
+                isPrime = true;
+                for (int n = 0;; n++) {
+                    int number = (n * n) + (a * n) + b;
+                    isPrime = true;
+                    if (number <= 2)
+                        continue;
+                    for (int i = 2; i < number / 2; i++) {
+                        if (number % i == 0) {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+                if (count > saveCount) {
+                    saveA = a;
+                    saveB = b;
+                    saveCount = count;
+                }
+            }
+        }
+        // Printing the final output, prouct of the coefficients in the equation
+        System.out.println("Product of coeffecients in the equation that produces the maximum number of primes for consecutive values of n is " + saveA * saveB);
     }
 }
